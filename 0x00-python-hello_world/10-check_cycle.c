cf45c3a54c03e6c2282882b4bbbd4a851b337e9f
j4ir0st <jairocast2@gmail.com>
2021-12-20 20:58:41 -0800

    checks if there is a cycle in a linked list with betty style

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,27 +1,25 @@
 #include "lists.h"
+
 /**
  * check_cycle - checks if a linked list contains a cycle
  * @list: linked list to check
  *
- * Return: 1 if the list has a cycle, 0 if it doesn't
+ * Return: 1 if the there is a cycle, 0 if not
  */
+
 int check_cycle(listint_t *list)
 {
 	listint_t *first = list;
 	listint_t *second = list;
 
 	if (!list)
-	{
 		return (0);
-	}
-	while (second->next)
+	while (first && second && second->next)
 	{
 		first = first->next;
 		second = second->next->next;
 		if (first == second)
-		{
 			return (1);
-		}
 	}
 	return (0);
 }
