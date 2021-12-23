31912d771430f65bfe0cd3342033ca470647e010
j4ir0st <jairocast2@gmail.com>
2021-12-23 09:56:50 -0800

    removing exit 0

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-import sys
 if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
 
     if (len(sys.argv) != 4):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
@@ -11,18 +11,14 @@ if __name__ == "__main__":
     op = sys.argv[2]
     b = int(sys.argv[3])
 
-    if (op == "+"):
+    if (op == '+'):
         print("{:d} {} {:d} = {:d}".format(a, op, b, add(a, b)))
-        sys.exit(0)
-    elif (op == "-"):
+    elif (op == '-'):
         print("{:d} {} {:d} = {:d}".format(a, op, b, sub(a, b)))
-        sys.exit(0)
-    elif (op == "*"):
+    elif (op == '*'):
         print("{:d} {} {:d} = {:d}".format(a, op, b, mul(a, b)))
-        sys.exit(0)
-    elif (op == "/"):
+    elif (op == '/'):
         print("{:d} {} {:d} = {:d}".format(a, op, b, div(a, b)))
-        sys.exit(0)
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
