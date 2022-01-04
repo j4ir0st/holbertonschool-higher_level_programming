e3608c7290d6113c76d05cc50b65330b2d13d185
j4ir0st <jairocast2@gmail.com>
2022-01-04 11:43:12 -0800

    prints numbers from 0 to 99 with two digist

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for c in range(0, 100):
     if (c != 99):
-        print("{}".format(c), end=", ")
+        print("{:02d}".format(c), end=", ")
     else:
-        print("{}".format(c))
+        print("{:02d}".format(c))
