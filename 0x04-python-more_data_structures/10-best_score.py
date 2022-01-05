4c9865c7b639bf6c610c7ecdd00eedf576692577
j4ir0st <jairocast2@gmail.com>
2022-01-05 12:53:28 -0800

    returns a key with the biggest integer value considering None and empty

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if (a_dictionary and a_dictionary != []):
+    if (a_dictionary):
         best = max(a_dictionary)
         return (best)
+    elif (a_dictionary == []):
+        return (None)
     else:
         return (None)
