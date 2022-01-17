dd63165ec191a53a308bf6790c875a383fae8c43
j4ir0st <jairocast2@gmail.com>
2022-01-17 12:57:26 -0800

    print the first x elements of a list

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    c = 0
+    for i in range(x):
+        try:
+            c += 1
+            print("{:d}".format(my_list[i]), end='')
+        except IndexError:
+            raise
+        except:
+            continue
+    print('')
+    return c
