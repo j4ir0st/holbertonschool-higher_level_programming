f59117aeae8fdf7be8047aba073c67c481fd3454
j4ir0st <jairocast2@gmail.com>
2022-01-17 15:09:50 -0800

    Prints an integer handling errors and with pycodestyle

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,5 +5,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except Exception as eer:
-        print("Exception: {}".format(eer), file = sys.stderr)
+        print("Exception: {}".format(eer), file=sys.stderr)
         return False
