85d30482de3d523a5f3937e18060eef81170c37d
j4ir0st <jairocast2@gmail.com>
2022-01-18 18:43:02 -0800

    creates a Square class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Creating a Square class
+Square: A class
+Atributtes: Size
+"""
+
+
+class Square:
+    """    Square Class with size atributte   """
+    def __init__(self, size):
+        self.__size = size
