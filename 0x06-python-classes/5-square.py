32a3ae626e116fd4584242006e7a9220a695d61d
j4ir0st <jairocast2@gmail.com>
2022-01-18 19:51:41 -0800

    returns the square and prints #

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Creating a Square class
+Square: A class
+Atributtes: Size
+"""
+
+
+class Square:
+    """    Square Class with size atributte   """
+    def __init__(self, size=0):
+        self.size = size
+
+    """    Square Class with size atributte   """
+    def area(self):
+        return (self.__size ** 2)
+
+    """    Square Class with size atributte   """
+    def my_print(self):
+        x = 0
+        y = 0
+        if (self.__size == 0):
+            print("")
+        else:
+            while x < self.__size:
+                while y < self.__size:
+                    print("#", end="")
+                    y += 1
+                print("")
+                x += 1
+                y = 0
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, s_size):
+        if not isinstance(s_size, int):
+            raise TypeError('size must be an integer')
+        if (s_size < 0):
+            raise ValueError('size must be >= 0')
+        self.__size = s_size
