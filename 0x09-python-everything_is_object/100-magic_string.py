7bc6fbe8444781028c7a6e5e548fe03350791ebd
j4ir0st <jairocast2@gmail.com>
2022-01-25 11:12:08 -0800

    Pythonic way

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    magic_string.count = 1 if "count" not in magic_string.__dict__ else magic_string.count + 1
-    return "BestSchool" if magic_string.count == 1 else "BestSchool" + (", BestSchool" * magic_string.count)
+def magic_string(m_s=[0]):
+    m_s += 1
+    return (str("BestSchool, " * (m_s[0] - 1)) + "BestSchool")
