a42120772a5ebcbc5b02c5f5ca4da794da293a1d
j4ir0st <jairocast2@gmail.com>
2022-01-25 16:02:44 -0800

    Pycodestyle style

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+"""
+Creating an empty locked class
+LockedClass: An empty class
+Atributtes: __slots__
+"""
+
+
 class LockedClass:
+    """    Empty Class    """
     __slots__ = ['first_name']
