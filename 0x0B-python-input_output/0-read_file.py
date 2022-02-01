713b66e3ff02748fe58762478aa713ce68fb66e1
j4ir0st <jairocast2@gmail.com>
2022-02-01 15:31:31 -0800

    Reads a file with pycodestyle

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" Read file """
+
+
 def read_file(filename=""):
+    """ Reads the file """
     with open(filename, 'r', encoding='UTF8') as fo:
         buff = fo.read()
     print(buff, end='')
