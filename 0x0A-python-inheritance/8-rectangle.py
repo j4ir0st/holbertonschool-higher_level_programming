3e1491d48bb64bd4bb77ec225f26567c741a35f2
j4ir0st <jairocast2@gmail.com>
2022-01-31 20:14:05 -0800

    Creating init def importing base geometry

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,17 +2,8 @@
 """ Inheritance class """
 
 
-class BaseGeometry:
-    """ BaseGeometry class """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-    def area(self):
-        raise Exception('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
 
 class Rectangle(BaseGeometry):
     """ Rectangle class """
