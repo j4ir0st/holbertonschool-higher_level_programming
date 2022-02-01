c1b373419f3d0460d4054906e01044f9322b4a39
j4ir0st <jairocast2@gmail.com>
2022-01-31 20:51:01 -0800

    Adds a new attribute to an object if it’s possible with pycodestyle

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -3,6 +3,7 @@
 
 
 def add_attribute(obj, att, value):
+    """ Adds a new attribute to an object if it’s possible """
     if hasattr(obj, "__dict__"):
         setattr(obj, att, value)
         return
