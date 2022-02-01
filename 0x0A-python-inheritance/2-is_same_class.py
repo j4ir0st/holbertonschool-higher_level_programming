34eabed03a5a4761b9abe3e0daeec2c6145ecf8b
j4ir0st <jairocast2@gmail.com>
2022-01-31 19:10:09 -0800

    pycodestyle

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Exact same object """
+
+
+def is_same_class(obj, a_class):
+    """ Returns the exact same class """
+    if (type(obj) == a_class):
+        return (True)
+    return (False)
