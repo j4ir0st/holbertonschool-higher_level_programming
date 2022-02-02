d483efe88e0c3b50d12512d60bce949aacaceec9
j4ir0st <jairocast2@gmail.com>
2022-02-01 20:57:28 -0800

    returns the dictionary description with simple data structure

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ Create object from a JSON file """
-import json
 
 
 def class_to_json(obj):
-    return (json.dumps(obj, default=lambda o: o.__dict__, sort_keys=True))
+    return (obj.__dict__)
