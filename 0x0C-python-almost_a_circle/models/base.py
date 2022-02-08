032dbead90e11b878f12202d07372a9eb56d5e61
j4ir0st <jairocast2@gmail.com>
2022-02-08 09:09:27 -0800

    base with pycodestyle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -8,8 +8,13 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-        if (id != None):
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects = Base.__nb_objects + 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        if (list_dictionaries is None or list_dictionaries == ""):
+            return ("[]")
+        return (json.dumps(list_dictionaries))
