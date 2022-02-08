7232383995b4e46d36d94a67ee06a338453885b2
j4ir0st <jairocast2@gmail.com>
2022-02-08 12:49:00 -0800

    Base adding class method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,3 +20,9 @@ class Base:
         if (list_dictionaries is None or list_dictionaries == ""):
             return ("[]")
         return (json.dumps(list_dictionaries))
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ Writes the given file """
+        with open(cls.__name__ + ".json", 'w', encoding='UTF8') as fo:
+            fo.write(json.dumps([elem.to_dictionary()for elem in list_objs]))
