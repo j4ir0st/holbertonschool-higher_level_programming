bc0a68fad1c178a4380a10748d20b9d0f2098127
j4ir0st <jairocast2@gmail.com>
2022-02-08 11:24:43 -0800

    square class with pycodestyle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,7 +10,37 @@ class Square(Rectangle):
         super().__init__(width=size, height=size, x=x, y=y, id=id)
 
     def __str__(self):
-        return ("[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width))
+        str2 = "[Square]"
+        str2 += " (" + str(self.id) + ") "
+        str2 += str(self.x) + "/" + str(self.y) + " - "
+        str2 += str(self.width)
+        return str2
+
+    def update(self, *args, **kwargs):
+        l_ar = len(args)
+        if not args or l_ar == 0:
+            for key, value in kwargs.items():
+                if key == "size":
+                    self.size = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
+                if key == "id":
+                    self.id = value
+        if (l_ar > 0):
+            self.id = args[0]
+        if (l_ar > 1):
+            self.size = args[1]
+        if (l_ar > 2):
+            self.x = args[2]
+        if (l_ar > 3):
+            self.y = args[3]
+
+    def to_dictionary(self):
+        d = dict([('x', self.x), ('y', self.y), ('id', self.id)])
+        d['size'] = self.width
+        return (d)
 
     @property
     def size(self):
@@ -18,7 +48,7 @@ class Square(Rectangle):
 
     @size.setter
     def size(self, v_size):
-        if  not (isinstance(v_size, int)):
+        if not (isinstance(v_size, int)):
             raise TypeError('width must be an integer')
         if (v_size <= 0):
             raise ValueError('width must be > 0')
