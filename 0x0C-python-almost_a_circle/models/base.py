b479e5d57d76162680f25c4a11b4691b1f6caced
j4ir0st <jairocast2@gmail.com>
2022-02-08 09:12:24 -0800

    base with pycodestyle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Base class """
-import json
+"""import json"""
 
 
 class Base:
@@ -14,7 +14,7 @@ class Base:
             Base.__nb_objects = Base.__nb_objects + 1
             self.id = Base.__nb_objects
 
-    def to_json_string(list_dictionaries):
+    """def to_json_string(list_dictionaries):
         if (list_dictionaries is None or list_dictionaries == ""):
             return ("[]")
-        return (json.dumps(list_dictionaries))
+        return (json.dumps(list_dictionaries))"""
