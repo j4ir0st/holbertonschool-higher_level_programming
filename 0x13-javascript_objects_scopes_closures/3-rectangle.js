b23d79245e166471efe3cc3558edd991ce97d047
j4ir0st <jairocast2@gmail.com>
2022-04-05 18:00:41 -0500

    Creating a print method

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -12,7 +12,7 @@ class Rectangle {
     for (let i = 0; i < this.width; i++) {
       subs = subs + 'X';
     }
-    for (let j = 0; j < this.width; j++) {
+    for (let j = 0; j < this.height; j++) {
       console.log(subs);
     }
   }
