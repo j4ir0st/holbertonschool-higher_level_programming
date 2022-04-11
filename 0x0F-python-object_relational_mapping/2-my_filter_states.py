d1015e25d8c365e4989ab72beac49fcaf57242ac
j4ir0st <jairocast2@gmail.com>
2022-04-11 10:16:04 -0500

    takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+script that lists all states from the database hbtn_0e_0_usa
+'''
+
+
+if __name__ == '__main__':
+    from sys import argv
+    import MySQLdb
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    cur = db.cursor()
+    cur.execute("""
+    SELECT *
+    FROM states
+    WHERE name LIKE BINARY '{}'
+    ORDER BY id
+    """.format(argv[4]))
+    pr = cur.fetchall()
+    for row in pr:
+        print(row)
