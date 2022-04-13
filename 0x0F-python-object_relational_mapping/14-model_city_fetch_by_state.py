d36bd75fa0dacc2c4e44fa2dad42ab40bf6005eb
j4ir0st <jairocast2@gmail.com>
2022-04-12 22:12:17 -0500

    prints all City objects from the database hbtn_0e_14_usa and its class with pycodestyle4

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -7,7 +7,6 @@ from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
-
 if __name__ == '__main__':
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost/{}'.format(
