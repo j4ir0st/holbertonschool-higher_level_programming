642431f4054969e509f5417d10c6a9c08e2e8415
j4ir0st <jairocast2@gmail.com>
2022-04-12 22:17:09 -0500

    prints all City objects from the database hbtn_0e_14_usa and its class with pycodestyle5

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -4,6 +4,7 @@ Script link class to table in database
 """
 import sys
 from model_state import Base, State
+from model_city import City
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
