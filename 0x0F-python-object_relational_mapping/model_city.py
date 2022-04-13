95648ca655c226b604145f19515f36a6cac3c663
j4ir0st <jairocast2@gmail.com>
2022-04-12 22:09:01 -0500

    prints all City objects from the database hbtn_0e_14_usa and its class with pycodestyle3

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -9,7 +9,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 class City(Base):
     """
-    Class: definition inherits from Base
+    Class: definition city class inherits from Base
     """
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, nullable=False)
