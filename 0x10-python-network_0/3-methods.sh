a18e8c8b7baf15d61c2a0744d72be644904a6cef
j4ir0st <jairocast2@gmail.com>
2022-04-21 21:20:54 -0500

    takes in a URL and displays all HTTP methods the server will accept delete DELETE

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL and displays all HTTP methods the server will accept.
-sudo curl $1 -I DELETE -s | grep "Allow" | cut -d ":" -f 2-
+sudo curl $1 -I -s | grep "Allow" | cut -d ":" -f 2-
