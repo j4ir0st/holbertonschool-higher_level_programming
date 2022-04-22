538ddb8bc7f3972683a5284f30922f68c0ed05c3
j4ir0st <jairocast2@gmail.com>
2022-04-21 23:28:47 -0500

    finds a peak in a list of unsorted integers

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Test function find_peak """
+
+
+def find_peak(list_of_integers):
+    """finds a peak in a list of unsorted integers"""
+    if not list_of_integers:
+        return (None)
+
+    size = len(list_of_integers)
+    half_size = int(length / 2)
+
+    if (half_size + 1 >= size and half_size - 1 < 0):
+        return (list_of_integers[half_size])
+    elif (half_size - 1 < 0):
+        if (list_of_integers[half_size] > list_of_integers[half_size + 1]):
+            return (list_of_integers[half_size])
+        else:
+            return (list_of_integers[half_size + 1])
+    elif (half_size + 1 >= size):
+        if (list_of_integers[half_size] > list_of_integers[half_size - 1]):
+            return (list_of_integers[half_size])
+        else:
+            return (list_of_integers[half_size - 1])
+
+    if (
+            list_of_integers[half_size - 1] < list_of_integers[half_size] /
+            > list_of_integers[half_size + 1]
+    ):
+        return (list_of_integers[half_size])
+
+    if (list_of_integers[half_size + 1] > list_of_integers[half_size - 1]):
+        return (find_peak(list_of_integers[half_size:]))
+
+    return (find_peak(list_of_integers[:half_size]))
