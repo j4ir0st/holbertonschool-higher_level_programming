fedd9e686cdddc8141a2c91704e9c2f91099ec2c
j4ir0st <jairocast2@gmail.com>
2022-04-21 20:09:57 -0500

    takes in a URL, sends a request to that URL, and displays the size of the body of the response in bytes with comments with shellcheck

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
+# sends a request to that URL and displays the size of the body of the response
 sudo curl $1 -I -s | grep "Content-Length" | cut -d " " -f 2
