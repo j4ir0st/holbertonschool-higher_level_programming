65c047e84742c4d128b3d3fc4346b9130c7f822d
j4ir0st <jairocast2@gmail.com>
2022-04-29 21:33:00 -0500

    creates the State “California” with the City “San Francisco” from the database hbtn_0e_100_usa class documented

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
- contains the class definition of a State and an instance Base
+Contains the class definition of a State and an instance Base
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
@@ -10,7 +10,7 @@ Base = declarative_base()
 
 
 class State(Base):
-    """ class definition inherits from Base """
+    """ Class definition inherits from Base """
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, nullable=False)
