c1c345f8bf71b19e4cffb1463c2cfca147e518f7
j4ir0st <jairocast2@gmail.com>
2022-05-17 14:07:33 -0500

    reads and prints the content of a filewith semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs').promises;
 
-fs.readFile(process.argv[2])
-    .then((data) => console.log(data.toString()))
-    .catch((err) => console.error(err));
+fs.readFile(process.argv[2], { encoding: 'utf-8' })
+  .then((data) => console.log(data.toString()))
+  .catch((err) => console.error(err));
