524159e7459196cd20276096c2b24e53ffa9c6d6
j4ir0st <jairocast2@gmail.com>
2022-05-17 17:14:12 -0500

    gets the contents of a webpage and stores it in a file with semistandard

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,11 +2,10 @@
 const fs = require('fs');
 const axios = require('axios').default;
 
-
 axios.get(process.argv[2])
-    .then(response => {
-	fs.writeFile(process.argv[3], response.data, 'utf-8', err => {
-	    if (err) console.log(err);
-	});
-    })
-    .catch(err => console.log('code: ' + err.response.status));
+  .then(response => {
+    fs.writeFile(process.argv[3], response.data, 'utf-8', err => {
+      if (err) console.log(err);
+    });
+  })
+  .catch(err => console.log('code: ' + err.response.status));
