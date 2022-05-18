b4ae2f7a9a921e3968c80247aebb61c48e197cee
j4ir0st <jairocast2@gmail.com>
2022-05-17 22:33:34 -0500

    computes the number of tasks completed by user id

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,8 +1,18 @@
 #!/usr/bin/node
-const axios = require('axios').default;
+const axios = require('axios');
 
 axios.get(process.argv[2])
-    .then(response => {
-	console.log(response.data)
-    })
-  .catch(err => console.log('code: ' + err.response.status));
+  .then(resp => {
+    const list = {};
+    resp.data.forEach(task => {
+      if (task.completed === true) {
+        if (list[task.userId] === undefined) {
+          list[task.userId] = 1;
+        } else {
+          list[task.userId]++;
+        }
+      }
+    });
+    console.log(list);
+  })
+  .catch(err => console.log(err.message));
